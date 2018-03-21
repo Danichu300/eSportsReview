22648d999c3732012dc0bbd142ad3070b2556e1b
Danichu300 <danikillersx@gmail.com>
2018-03-21 14:24:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>eSports Review</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/mainstyle.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="img/iESR.ico" />
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="parallax"></div>
+        <nav>
+            <div class="logo_container">
+                <a href="#"><img src="img/csgologo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/overwatchlogo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/heroeslogo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/lollogo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/pubglogo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/rocketlogo.jpg"/></a> 
+            </div>
+
+            <div class="logo_container">
+                <a href="#"><img src="img/dotalogo.jpg"/></a> 
+            </div>
+        </nav>
+
+        <!--Footer-->
+        <footer class="page-footer font-small unique-color-dark pt-0">
+
+            <div style="background-color: #3E4551;">
+                <div class="container">
+                    <!--Grid row-->
+                    <div class="row py-4 d-flex align-items-center">
+
+                        <!--Grid column-->
+                        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                            <h6 class="mb-0 white-text">¡Comparte en tus redes sociales!</h6>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-md-6 col-lg-7 text-center text-md-right">
+                            <!--Facebook-->
+                            <a class="fb-ic ml-0">
+                                <i class="fa fa-facebook white-text mr-lg-4"> </i>
+                            </a>
+                            <!--Twitter-->
+                            <a class="tw-ic">
+                                <i class="fa fa-twitter white-text mr-lg-4"> </i>
+                            </a>
+                            <!--Google +-->
+                            <a class="gplus-ic">
+                                <i class="fa fa-google-plus white-text mr-lg-4"> </i>
+                            </a>
+                            <!--Linkedin-->
+                            <a class="li-ic">
+                                <i class="fa fa-linkedin white-text mr-lg-4"> </i>
+                            </a>
+                            <!--Instagram-->
+                            <a class="ins-ic">
+                                <i class="fa fa-instagram white-text mr-lg-4"> </i>
+                            </a>
+                        </div>
+                        <!--Grid column-->
+
+                    </div>
+                    <!--Grid row-->
+                </div>
+            </div>
+
+            <!--Footer Links-->
+            <div class="container mt-5 mb-4 text-center text-md-left">
+                <div class="row mt-3">
+
+                    <!--First column-->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <strong>Novedades</strong>
+                        </h6>
+                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p>Conoce cada detalle sobre los jugadores, torneos, partidos, regiones y equipos que más te gusten.</p>
+                    </div>
+                    <!--/.First column-->
+
+                    <!--Second column-->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <strong>Próximamente</strong>
+                        </h6>
+                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p>
+                            <a href="#!">Próximos partidos</a>
+                        </p>
+                        <p>
+                            <a href="#!">Torneos cercanos</a>
+                        </p>
+                        <p>
+                            <a href="#!">Equipos destacados</a>
+                        </p>
+                    </div>
+                    <!--/.Second column-->
+
+                    <!--Third column-->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <strong>Aprende sus tácticas</strong>
+                        </h6>
+                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p>
+                            <a href="#!">Descripciones de videojuegos</a>
+                        </p>
+                        <p>
+                            <a href="#!">Funcionamiento de las partidas</a>
+                        </p>
+                        <p>
+                            <a href="#!">Mecánicas</a>
+                        </p>
+                        <p>
+                            <a href="#!">Y mucho más...</a>
+                        </p>
+                    </div>
+                    <!--/.Third column-->
+
+                    <!--Fourth column-->
+                    <div class="col-md-4 col-lg-3 col-xl-3">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <strong>Contacta con nosotros</strong>
+                        </h6>
+                        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p>
+                            <i class="fa fa-envelope mr-3"></i> danikillersx@gmail.com</p>
+                        <p>
+                            <i class="fa fa-envelope mr-3"></i> onesoco138@gmail.com</p>
+                        <p>
+                            <i class="fa fa-envelope mr-3"></i> justcyclo@gmail.com</p>
+                    </div>
+                    <!--/.Fourth column-->
+
+                </div>
+            </div>
+            <!--/.Footer Links-->
+
+            <!-- Copyright-->
+            <div class="footer-copyright py-3 text-center">
+                © 2018 Copyright: Daniel Rodríguez Cardell, Antonio García Llabrés, Julián Maestre Cabrera
+            </div>
+            <!--/.Copyright -->
+
+        </footer>
+        <!--/.Footer-->
+    </body>
+</html>
