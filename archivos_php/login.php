abb793582384fbd5b3dba6275cc93cf3ff49348e
Danichu300 <danikillersx@gmail.com>
2018-04-11 15:33:41 +0200

    Update login.php

--- archivos_php/login.php
+++ archivos_php/login.php
@@ -11,19 +11,21 @@ require ("./conexion.php");
   echo "<script>alert('hola');</script>";
   } */
 
-if (isset($_POST['login_button'])) {
-    $user_email = trim($_POST['id']);
-    $user_password = trim($_POST['pass']);
+//if (isset($_POST['login_button'])) {
+    $user_email = ($_POST['id']);
+    $user_password = ($_POST['pass']);
     $sql = "SELECT * FROM usuario WHERE id_usuario=" . $user_email;
     //$resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
     //$row = mysqli_fetch_assoc($resultset);
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
     if ($row['password'] == $user_password) {
-        echo "ok";
+        echo "<p>ok</p>";
         $_SESSION['user_session'] = $row['id_usuario'];
     } else {
-        echo "email or password does not exist.";
+        echo "<p>email or password does not exist.</p>";
     }
-}
+//} else {
+   // echo "<p>vaya hombre :/";
+//}
 ?>
