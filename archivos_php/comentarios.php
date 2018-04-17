4f3053bac24ff80f74083dea8871613dc9c2d075
Danichu300 <danikillersx@gmail.com>
2018-04-17 11:20:57 +0200

    Create comentarios.php

---
+++ archivos_php/comentarios.php
@@ -0,0 +1,38 @@
+<?php
+
+//guardarComentario();
+//function guardarComentario() {
+require './conexion.php';
+
+$user = $_POST['user_comment'];
+$texto = $_POST['text_comment'];
+$videojuego = $_POST['videojuego'];
+
+$buscarUsuario = "SELECT id_usuario FROM usuario WHERE nombre_usuario = '" . $user . "'";
+
+$result = $conn->query($buscarUsuario);
+
+$row = $result->fetch_array();
+
+if ($result) {
+
+    $sql = "INSERT INTO comentarios (id_comentario, usuario, contenido, videojuego, fecha_comentario) VALUES (null," . $row['id_usuario'] . ",'" . $texto . "','" . $videojuego . "',CURRENT_TIMESTAMP())";
+
+    $resultado = $conn->query($sql);
+
+    if ($resultado) {
+        echo "Todo guay";
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
+header("Location: ../noticias/csgo.php#formularioComentarios");
+
+
+//}
+
+/* function cargarComentarios() {
+  $sql = "SELECT usuario, contenido FROM comentarios ORDER BY fecha_comentario";
+  } */
+?>
