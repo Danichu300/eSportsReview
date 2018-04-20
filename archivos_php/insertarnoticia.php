311acba0183638f54ba1f3e2ee5c0924fb92873b
Danichu300 <danikillersx@gmail.com>
2018-04-20 14:58:08 +0200

    Create insertarnoticia.php

---
+++ archivos_php/insertarnoticia.php
@@ -0,0 +1,26 @@
+<?php
+
+require("./conexion.php");
+
+$titulo = $_POST['titulo_noticia'];
+$contenido = $_POST['contenido_noticia'];
+if(isset($_POST['imagen_noticia'])){
+    $imagen = $_POST['imagen_noticia'];
+} else {
+    $imagen = null;
+}
+
+$videojuego = $_POST['videojuego_noticia'];
+$admin = $_POST['autorizacion'];
+
+$sql = "INSERT INTO noticia (id_noticia,admin,titulo,contenido,imagen,videojuego) VALUES (null," . $admin . ",'" . $titulo . "','" . $contenido . "','" . $imagen . "','" . $videojuego . "')";
+
+$result = $conn->query($sql);
+
+if($result){
+    echo "yeeeeeee";
+} else {
+    echo mysqli_error($conn);
+}
+
+?>
