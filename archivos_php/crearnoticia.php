85ecf61427bbda52e002fe184087bbb4d3e11064
Danichu300 <danikillersx@gmail.com>
2018-04-24 12:23:07 +0200

    Update crearnoticia.php

--- archivos_php/crearnoticia.php
+++ archivos_php/crearnoticia.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="../css/newsstyle.css">
-        <link rel="stylesheet" href="../css/crearnoticia.css"/>
+        <link rel="stylesheet" type="text/css" href="../css/crearnoticia.css"/>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
